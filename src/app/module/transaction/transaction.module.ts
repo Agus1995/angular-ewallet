cd583d0e653c2125d1af6e71a5ef4dd9e366f4da
danis gusti <danisgusti7@gmail.com>
2019-05-30 14:40:27 +0700

    add transaction

--- src/app/module/transaction/transaction.module.ts
+++ src/app/module/transaction/transaction.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { TransactionRoutingModule } from './transaction-routing.module';
 import { TransactionComponent } from 'src/app/component/transaction/transaction.component';
+import { TopUpComponent } from 'src/app/component/top-up/top-up.component';
+import { TransferComponent } from 'src/app/component/transfer/transfer.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    TransactionComponent
+    TransactionComponent,
+    TopUpComponent,
+    TransferComponent,
   ],
   imports: [
     CommonModule,
