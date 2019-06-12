b5e5d1f224524ef533459fc8bb012d77c4b9a9ee
danis gusti <danisgusti7@gmail.com>
2019-06-12 08:51:26 +0700

    add chart

--- src/app/module/forex/forex-routing.module.ts
+++ src/app/module/forex/forex-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ForexComponent } from 'src/app/component/forex/forex.component';
+import { ChartExComponent } from 'src/app/component/chart-ex/chart-ex.component';
 
 const routes: Routes = [
-  {path:"", component: ForexComponent}
+  {path:"", component: ForexComponent},
+  {path:"charts", component: ChartExComponent},
+  {path:"**", component: ChartExComponent}
 ];
 
 @NgModule({
