173a6456e337100870a13b0dcf4c1d216dc3d58d
danis gusti <danisgusti7@gmail.com>
2019-06-13 10:51:53 +0700

    fix bug

--- src/app/component/account/account.component.ts
+++ src/app/component/account/account.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {Router} from '@angular/router';
-
+import { AccountService } from 'src/app/service/account.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Account } from 'src/app/model/account';
+import { Customer } from 'src/app/model/customer';
+ 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -8,11 +12,63 @@ import {Router} from '@angular/router';
 })
 export class AccountComponent implements OnInit {
 
-  constructor(
-    private  router: Router
+  constructor(private router: Router, private service: AccountService, private fb: FormBuilder
   ) { }
+  formAccount: FormGroup;
+  cif1: Customer = new Customer();
+  accounts: Account[] = [];
+  cif = localStorage.getItem('cif');
 
   ngOnInit() {
+    this.getAccount();
+    this.cif1.cif = this.cif
+
+
+    this.formAccount = this.fb.group({
+      name: ['', Validators.required],
+      curencyType: ['', Validators.required]
+    });
+
+  }
+
+
+  // @Input()
+  account: Account = {
+    accountNumber: '',
+    name:'',
+    curencyType:'',
+    balance: 0,
+    createdAt:'',
+    updatedAt:'',
+    customer: this.cif1
+
+  }
+
+  async createAccount(){
+    this.account.name = this.formAccount.controls["name"].value;
+    this.account.curencyType = this.formAccount.controls["curencyType"].value;
+    console.log(this.account);
+    const response = await this.service.createAccount(this.account).toPromise();
+    if(response.responsecode != 1){
+      alert(response.responsemessage)
+    } else{
+      alert("success");
+      this.getAccount();
+    }
+  }
+
+
+  async getAccount(){
+    if(this.cif == undefined){
+      this.cif = localStorage.getItem(this.cif);
+    }
+    const response = await this.service.getAccount(this.cif).toPromise();
+    if(response.responsecode != 1){
+      alert(response.responsemessage)
+    } else{
+      console.log(response.data)
+      this.accounts= response.data;
+    }
   }
 
   walletClick(){
--- src/app/service/transaction.service.ts
+++ src/app/service/transaction.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Transaction } from '../model/transaction';
+import { Observable } from 'rxjs';
+import { CommonResponse } from '../security/commonResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { } 
+
+  urlTopUp = "http://localhost:9191/transaction";
+
+  topUp(trans: Transaction) : Observable<CommonResponse<Transaction[]>>{
+    return this.http.post<CommonResponse<Transaction[]>>(this.urlTopUp, trans)
+  }
 }
