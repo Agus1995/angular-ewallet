599daa0329f28bcc86a42d9d9dd64da4908cc1ae
Agus1995 <aguswhibawa95@gmail.com>
2019-06-17 07:45:33 +0700

    add api

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -11,9 +11,9 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  urlLogin = 'http://localhost:9191/login';
-  urlRegister = 'http://localhost:9191/register';
-  urlProfile = 'http://localhost:9191/customer/';
+  urlLogin = 'http://localhost:9191/api/login';
+  urlRegister = 'http://localhost:9191/api/register';
+  urlProfile = 'http://localhost:9191/api/customer/';
  
 
 login(login: Customer) : Observable<CommonResponse<Customer>>{
