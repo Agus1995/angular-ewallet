a3fa873640b89711c5cb8a2a930e9fa417b65bb0
Agus1995 <aguswhibawa95@gmail.com>
2019-06-17 11:36:36 +0700

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ import {AuthenticationGuardService} from './security/authentication-guard.servic
 import { KursComponent } from './component/kurs/kurs.component';
 
 const routes: Routes = [
-  {path: "", redirectTo:"customer/profile",pathMatch:"full"},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path:"account", loadChildren: './module/account/account.module#AccountModule', canActivate: [AuthenticationGuardService]},
   {path:'wallet', loadChildren: './module/wallet/wallet.module#WalletModule', canActivate: [AuthenticationGuardService]},
   {path:"forex", loadChildren: './module/forex/forex.module#ForexModule', canActivate: [AuthenticationGuardService]},
@@ -39,4 +39,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
- 
