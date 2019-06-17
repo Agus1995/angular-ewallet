4cfc33e97f62ffb36ee74333cffc362d8e4436b2
Agus1995 <aguswhibawa95@gmail.com>
2019-06-17 14:35:24 +0700

    update auth service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Customer } from '../model/customer';
 import { Observable } from 'rxjs';
 import { CommonResponse } from '../security/commonResponse';
-import {Constants} from "./Constants";
+import {Constants} from './Constants';
 
 @Injectable({
   providedIn: 'root'
@@ -12,24 +12,15 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-login(login: Customer) : Observable<CommonResponse<Customer>> {
-  return this.http.post<CommonResponse<Customer>>(`${Constants.API_BASE_URL}/login`, login);
-}
-
-// isLogin(){
-//   if(localStorage.getItem("user") !== null){
-//     return false;
-//   } else {
-//     return true;
-//   }
-
-register(register: Customer): Observable<CommonResponse<Customer>>{
-  return this.http.post<CommonResponse<Customer>>(`${Constants.API_BASE_URL}/register`, register);
-}
-
-profile(cif): Observable<CommonResponse<Customer>>{
-  return this.http.get<CommonResponse<Customer>>(`${Constants.API_BASE_URL}/` + cif);
-}
+  login(login: Customer): Observable<CommonResponse<Customer>> {
+    return this.http.post<CommonResponse<Customer>>(`${Constants.API_BASE_URL}/login`, login);
+  }
+  register(register: Customer): Observable<CommonResponse<Customer>> {
+    return this.http.post<CommonResponse<Customer>>(`${Constants.API_BASE_URL}/register`, register);
+  }
+  profile(cif): Observable<CommonResponse<Customer>> {
+    return this.http.get<CommonResponse<Customer>>(`${Constants.API_BASE_URL}/` + cif);
+  }
 
 }
 
